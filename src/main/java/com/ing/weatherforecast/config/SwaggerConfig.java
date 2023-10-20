a1b6413db6614dbf5da0303aa1bab9c56416ef0d
tariksahin <tariksahin01@gmail.com>
2023-10-20 23:31:58 +0300

    call values from application properties

--- src/main/java/com/ing/weatherforecast/config/SwaggerConfig.java
+++ src/main/java/com/ing/weatherforecast/config/SwaggerConfig.java
@@ -4,17 +4,18 @@ package com.ing.weatherforecast.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
 public class SwaggerConfig {
+    @Value("${swagger.api.title}")
+    private String title;
 
-    private String title = "Weather project API";
-
-
-    private String description = "Weather project";
+    @Value("${swagger.api.description}")
+    private String description;
 
     @Bean
     public OpenAPI customOpenApi(){
--- src/main/java/com/ing/weatherforecast/service/WeatherService.java
+++ src/main/java/com/ing/weatherforecast/service/WeatherService.java
@@ -1,55 +1,56 @@
-    package com.ing.weatherforecast.service;
+package com.ing.weatherforecast.service;
 
-    import com.ing.weatherforecast.exception.WeatherForecastCouldntReceivedException;
-    import com.ing.weatherforecast.model.WeatherApiResponse;
-    import com.ing.weatherforecast.model.WeatherForecast;
-    import org.springframework.stereotype.Service;
-    import org.springframework.web.client.RestTemplate;
-    import org.springframework.web.util.UriComponentsBuilder;
+import com.ing.weatherforecast.exception.WeatherForecastCouldntReceivedException;
+import com.ing.weatherforecast.model.WeatherApiResponse;
+import com.ing.weatherforecast.model.WeatherForecast;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-    import java.time.LocalDate;
+import java.time.LocalDate;
 
-    @Service
-    public class WeatherService {
+@Service
+public class WeatherService {
 
-        private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-        public WeatherService(RestTemplate restTemplate) {
-            this.restTemplate = restTemplate;
-        }
-
-
-        private String apiKey = "5248854d717678e18afe5121cd9caf90";
-
-
-        private String baseUrl = "https://api.openweathermap.org";
-
-        public WeatherForecast getWeatherForecast(String city) {
-
-            String url = UriComponentsBuilder.fromUriString(baseUrl).path("/data/2.5/weather").queryParam("q", city).queryParam("appid", apiKey).toUriString();
-            // String url = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=5248854d717678e18afe5121cd9caf90"; //for test
-
-            WeatherApiResponse response;
-            try {
-                response = restTemplate.getForObject(url, WeatherApiResponse.class);
-            } catch (Exception ex) {
-                throw new WeatherForecastCouldntReceivedException(city);
-            }
-            if (response != null) {
-                WeatherForecast weatherForecast = new WeatherForecast(
-                        response.getName(),
-                        LocalDate.now(),
-                        response.getMain().getTemp_max(),
-                        response.getMain().getFeels_like(),
-                        response.getMain().getHumidity()
-                );
-                return weatherForecast;
-            } else {
-                return null;
-            }
-
-        }
+    public WeatherService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
+
+    @Value("${openweather.api.key}")
+    private String apiKey;
+
+    @Value("${openweather.api.base-url}")
+    private String baseUrl;
+
+    public WeatherForecast getWeatherForecast(String city) {
+
+        String url = UriComponentsBuilder.fromUriString(baseUrl).path("/data/2.5/weather").queryParam("q", city).queryParam("appid", apiKey).toUriString();
+        // String url = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=5248854d717678e18afe5121cd9caf90"; //for test
+
+        WeatherApiResponse response;
+        try {
+            response = restTemplate.getForObject(url, WeatherApiResponse.class);
+        } catch (Exception ex) {
+            throw new WeatherForecastCouldntReceivedException(city);
+        }
+        if (response != null) {
+            WeatherForecast weatherForecast = new WeatherForecast(
+                    response.getName(),
+                    LocalDate.now(),
+                    response.getMain().getTemp_max(),
+                    response.getMain().getFeels_like(),
+                    response.getMain().getHumidity()
+            );
+            return weatherForecast;
+        } else {
+            return null;
+        }
+
+    }
+}
 
 
 
